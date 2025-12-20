895fb5ca977b8e38d36c50f4fc53f92072303c34
blockmath <bmath2048@gmail.com>
2025-12-20 00:03:54 -0800

    .

--- build.sh
+++ build.sh
@@ -2,3 +2,7 @@ cargo build --target x86_64-apple-darwin
 cargo build --target x86_64-apple-darwin --release
 cargo build --target aarch64-apple-darwin
 cargo build --target aarch64-apple-darwin --release
+cargo build --target x86_64-pc-windows-gnu
+cargo build --target x86_64-pc-windows-gnu --release
+cargo build --target x86_64-unknown-linux-gnu
+cargo build --target x86_64-unknown-linux-gnu --release
